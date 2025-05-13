cb5ce945a3bb63f0033383c71f0555022b3e90a1
Uma Shankar Bharawa <ubharawa@gmail.com>
2025-05-14 00:33:19 +0530

    Enhance WaveComponent layout and image styles for improved visual appeal and responsiveness

--- src/components/WaveComponent.jsx
+++ src/components/WaveComponent.jsx
@@ -4,22 +4,15 @@ const WaveComponent = () => {
   return (
     <div className='relative'>
 
-      <div className="absolute grid grid-cols-8 grid-rows-10 w-full h-full max-md:hidden ">
-        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-9 2xl:mt-6 self-center justify-self-center max-xl:w-16 max-xl:h-12"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-8 col-start-2 self-end justify-self-center max-xl:w-16 max-xl:h-12"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-8 col-start-3 2xl:mt-8 self-center justify-self-center max-xl:w-16 max-xl:h-12"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-8 col-start-4 2xl:mt-8 justify-self-center max-xl:w-16 max-xl:h-12"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-7 col-start-5 self-end justify-self-center max-xl:w-16 max-xl:h-12"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-7 col-start-6 2xl:mt-7 self-center justify-self-center max-xl:w-16 max-xl:h-12"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-7 col-start-7 2xl:mt-8 justify-self-center max-xl:w-16 max-xl:h-12"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-6 col-start-8 2xl:mt-12 justify-self-center self-center max-xl:w-16 max-xl:h-12"/>
-        {/* <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20"/> */}
+      <div className="absolute grid grid-cols-8 grid-rows-40 w-full h-full max-md:hidden ">
+        <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20 row-span-6 row-start-35 max-2xl:row-start-32 justify-self-center max-xl:w-20 max-xl:h-16 max-lg:w-16 max-lg:h-12 transition-transform duration-300 transform hover:scale-125 "/>
+        <img src={HTMLIcon} alt="HTML Icon" className="w-24 h-19 row-span-6 row-start-33 max-2xl:row-start-31 col-start-2 justify-self-center max-xl:w-19 max-xl:h-15 max-lg:w-15 max-lg:h-11 transition-transform duration-300 transform hover:scale-125"/>
+        <img src={HTMLIcon} alt="HTML Icon" className="w-23 h-18 row-span-6 row-start-29 col-start-3 2xl:mt-8 justify-self-center max-xl:w-18 max-xl:h-14 max-lg:w-14 max-lg:h-10 transition-transform duration-300 transform hover:scale-125"/>
+        <img src={HTMLIcon} alt="HTML Icon" className="w-22 h-17 row-span-6 row-start-27 col-start-4 2xl:mt-8 justify-self-center self-center max-xl:w-17 max-xl:h-13 max-lg:w-13 max-lg:h-9 transition-transform duration-300 transform hover:scale-125"/>
+        <img src={HTMLIcon} alt="HTML Icon" className="w-21 h-16 row-span-6 row-start-27 max-2xl:row-start-25 col-start-5 justify-self-center max-xl:w-16 max-xl:h-12 max-lg:w-12 max-lg:h-8 transition-transform duration-300 transform hover:scale-125"/>
+        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-6 row-start-23 col-start-6 2xl:mt-7 justify-self-center max-xl:w-15 max-xl:h-11 max-lg:w-11 max-lg:h-7 transition-transform duration-300 transform hover:scale-125"/>
+        <img src={HTMLIcon} alt="HTML Icon" className="w-19 h-14 row-span-6 row-start-22 col-start-7 2xl:mt-8 justify-self-center max-xl:w-14 max-xl:h-10 max-lg:w-10 max-lg:h-6 transition-transform duration-300 transform hover:scale-125"/>
+        <img src={HTMLIcon} alt="HTML Icon" className="w-18 h-13 row-span-6 row-start-21 max-2xl:row-start-22 col-start-8 2xl:mt-12 justify-self-center max-xl:w-13 max-xl:h-9 max-lg:w-9 max-lg:h-5 transition-transform duration-300 transform hover:scale-125"/>
       </div>
 
       <svg
@@ -30,23 +23,23 @@ const WaveComponent = () => {
       >
         <path
           fill='#ffffff' // Ensure this is set to white
-          d='M0,500 C400,400 1200,400 1440,320 L1440,500 L0,510 Z'
+          d='M0,490 C500,500 1200,280 1440,340 L1440,500 L0,510 Z'
         />
         <path
           fill='#b6b0b8' // Light gray
-          d='M0,500 C400,420 1000,420 1440,340 L1440,500 L0,500 Z'
+          d='M0,500 C450,490 1000,300 1440,360 L1440,500 L0,500 Z'
         />
         <path
           fill='#5e5b5f' // Medium gray
-          d='M0,500 C400,440 1000,440 1440,360 L1440,500 L0,500 Z'
+          d='M0,500 C450,510 1000,320 1440,380 L1440,500 L0,500 Z'
         />
         <path
           fill='#2c2a2d' // Dark gray
-          d='M0,500 C400,460 1000,460 1440,380 L1440,500 L0,500 Z'
+          d='M0,500 C450,530 1000,340 1440,400 L1440,500 L0,500 Z'
         />
         <path
           fill='#1a1a1a' // Modified bottom black wave to a slightly lighter shade
-          d='M0,500 C400,480 1000,480 1440,400 L1440,500 L0,500 Z'
+          d='M0,500 C450,550 1000,360 1440,420 L1440,500 L0,500 Z'
         />
       </svg>
     </div>
