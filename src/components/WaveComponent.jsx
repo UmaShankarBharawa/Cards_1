b2184e6b93d215d1829174d498b2424e87dbff82
Uma Shankar Bharawa <ubharawa@gmail.com>
2025-05-13 18:39:36 +0530

    Refactor WaveComponent layout and image styles for improved responsiveness

--- src/components/WaveComponent.jsx
+++ src/components/WaveComponent.jsx
@@ -4,15 +4,15 @@ const WaveComponent = () => {
   return (
     <div className='relative'>
 
-      <div className="absolute grid grid-cols-8 grid-rows-10 w-full h-full max-lg:hidden ">
-        <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20 row-span-2 row-start-9 self-center justify-self-center max-xl:w-20 max-xl:h-16"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20 row-span-2 row-start-8 col-start-2 self-end justify-self-center max-xl:w-20 max-xl:h-16"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20 row-span-2 row-start-8 col-start-3 self-center justify-self-center max-xl:w-20 max-xl:h-16"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20 row-span-2 row-start-8 col-start-4 justify-self-center max-xl:w-20 max-xl:h-16"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20 row-span-2 row-start-7 col-start-5 self-end justify-self-center max-xl:w-20 max-xl:h-16"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20 row-span-2 row-start-7 col-start-6 self-center justify-self-center max-xl:w-20 max-xl:h-16"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20 row-span-2 row-start-7 col-start-7 justify-self-center max-xl:w-20 max-xl:h-16"/>
-        <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20 row-span-2 row-start-6 col-start-8 justify-self-center self-center max-xl:w-20 max-xl:h-16"/>
+      <div className="absolute grid grid-cols-8 grid-rows-10 w-full h-full max-md:hidden ">
+        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-9 2xl:mt-6 self-center justify-self-center max-xl:w-16 max-xl:h-12"/>
+        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-8 col-start-2 self-end justify-self-center max-xl:w-16 max-xl:h-12"/>
+        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-8 col-start-3 2xl:mt-8 self-center justify-self-center max-xl:w-16 max-xl:h-12"/>
+        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-8 col-start-4 2xl:mt-8 justify-self-center max-xl:w-16 max-xl:h-12"/>
+        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-7 col-start-5 self-end justify-self-center max-xl:w-16 max-xl:h-12"/>
+        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-7 col-start-6 2xl:mt-7 self-center justify-self-center max-xl:w-16 max-xl:h-12"/>
+        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-7 col-start-7 2xl:mt-8 justify-self-center max-xl:w-16 max-xl:h-12"/>
+        <img src={HTMLIcon} alt="HTML Icon" className="w-20 h-15 row-span-2 row-start-6 col-start-8 2xl:mt-12 justify-self-center self-center max-xl:w-16 max-xl:h-12"/>
         {/* <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20"/>
         <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20"/>
         <img src={HTMLIcon} alt="HTML Icon" className="w-25 h-20"/>
